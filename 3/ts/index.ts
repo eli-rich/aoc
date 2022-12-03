18f5164406e8afbb2ae8884638b73d0287aafe5a
Eli Richardson <elijahrich02@icloud.com>
2022-12-02 23:20:51 -0600

    day 3 ts

--- 3/ts/index.ts
+++ 3/ts/index.ts
@@ -1,3 +1,67 @@
 import { readFile } from 'node:fs/promises';
 
+const lower = 'abcdefghijklmnopqrstuvwxyz';
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
 const input = await readFile('input.txt', 'utf-8');
+const lines = input.split('\n');
+
+const findItems = (first: string, second: string) => {
+  const items: string[] = [];
+  for (const letter of first) {
+    if (second.includes(letter)) {
+      items.push(letter);
+    }
+  }
+  return Array.from(new Set(items));
+};
+
+const priorities: string[] = [];
+for (const line of lines) {
+  const first = line.slice(0, line.length / 2);
+  const second = line.slice(line.length / 2, line.length);
+  const p = findItems(first, second)[0];
+  priorities.push(p);
+}
+
+const result = priorities.reduce((sum, p) => {
+  if (p.toLowerCase() === p) {
+    return sum + lower.indexOf(p) + 1;
+  }
+  return sum + upper.indexOf(p) + 27;
+}, 0);
+
+console.log(result);
+
+const findItems2 = (groups: string[]) => {
+  const first = groups[0];
+  const second = groups[1];
+  const third = groups[2];
+
+  const items: string[] = [];
+  // iterate over group with most items by length
+  const longest = [first, second, third].sort((a, b) => b.length - a.length)[0];
+  for (const letter of longest) {
+    if (second.includes(letter) && third.includes(letter) && first.includes(letter)) {
+      items.push(letter);
+    }
+  }
+  return Array.from(new Set(items));
+};
+
+const priorities2: string[] = [];
+for (let i = 0; i < lines.length; i++) {
+  const group = [lines[i], lines[i + 1], lines[i + 2]];
+  i += 2;
+  const p = findItems2(group)[0];
+  priorities2.push(p);
+}
+
+const result2 = priorities2.reduce((sum, p) => {
+  if (p.toLowerCase() === p) {
+    return sum + lower.indexOf(p) + 1;
+  }
+  return sum + upper.indexOf(p) + 27;
+}, 0);
+
+console.log(result2);
