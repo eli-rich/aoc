f760d1f050d13a946efd35997f74a3f92cf346a0
Eli Richardson <elijahrich02@icloud.com>
2022-12-03 13:10:53 -0600

    improve performance for day 1

--- go/day1/one.go
+++ go/day1/one.go
@@ -2,7 +2,6 @@ package day1
 
 import (
 	_ "embed"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -12,19 +11,26 @@ var input string
 
 func Execute() (int, int) {
 	arr := strings.Split(string(input), "\n")
-	var totals []int
+	top := 0
+	top2 := 0
+	top3 := 0
 	var current int
 	for _, v := range arr {
 		if v == "" {
-			totals = append(totals, current)
+			if current > top {
+				top = current
+			} else if current > top2 {
+				top2 = current
+			} else if current > top3 {
+				top3 = current
+			}
 			current = 0
 			continue
 		}
 		val, _ := strconv.Atoi(v)
 		current += val
 	}
-	sort.Sort(sort.Reverse(sort.IntSlice(totals)))
-	part1 := totals[0]
-	part2 := totals[0] + totals[1] + totals[2]
+	part1 := top
+	part2 := top + top2 + top3
 	return part1, part2
 }
