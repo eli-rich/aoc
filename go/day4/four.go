3b5d1bbf0b34250646ebd7316f64acc85abff05d
Eli Richardson <elijahrich02@icloud.com>
2022-12-04 16:55:36 -0600

    use fmt.Scanf

--- go/day4/four.go
+++ go/day4/four.go
@@ -2,10 +2,8 @@ package day4
 
 import (
 	_ "embed"
-	"strconv"
+	"fmt"
 	"strings"
-
-	"github.com/eli-rich/aocutils"
 )
 
 //go:embed input.txt
@@ -16,14 +14,8 @@ func Execute() (int, int) {
 	part1 := 0
 	part2 := 0
 	for _, line := range lines {
-		splitComma := strings.Split(line, ",")
-		splitDash := aocutils.ArrayMap(splitComma, func(s string) []string {
-			return strings.Split(s, "-")
-		})
-		leftStart, _ := strconv.ParseInt(splitDash[0][0], 10, 32)
-		leftEnd, _ := strconv.ParseInt(splitDash[0][1], 10, 32)
-		rightStart, _ := strconv.ParseInt(splitDash[1][0], 10, 32)
-		rightEnd, _ := strconv.ParseInt(splitDash[1][1], 10, 32)
+		var leftStart, leftEnd, rightStart, rightEnd int
+		fmt.Sscanf(line, "%d-%d,%d-%d", &leftStart, &leftEnd, &rightStart, &rightEnd)
 		if (leftStart <= rightStart && leftEnd >= rightEnd) || (rightStart <= leftStart && rightEnd >= leftEnd) {
 			part1++
 		}
