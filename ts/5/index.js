c3578f6a2073756e9cb68646a058fc09092aef33
Eli Richardson <elijahrich02@icloud.com>
2022-12-05 11:38:33 -0600

    improve js solution

--- ts/5/index.js
+++ ts/5/index.js
@@ -1,62 +1,47 @@
-import { readFile } from 'node:fs/promises';
+import { readFileSync as r } from 'node:fs';
 
-const input = await readFile('input.txt', 'utf-8');
-const lines = input.split('\n');
+const [stackInput, stepInput] = r('./input.txt', 'utf-8').replaceAll('\r', '').split('\n\n');
 
-const stacks = [
-  [...'QHCTNSVB'],
-  [...'GBDQ'],
-  [...'BQSTRWF'],
-  [...'NDJZSWGL'],
-  [...'FVDPM'],
-  [...'JWF'],
-  [...'VJBQNL'],
-  [...'NSQJCRTG'],
-  [...'MDWCQSJ'],
-];
+// THANK YOU LEBSTER
+const stacks = stackInput
+  .split('\n')
+  .slice(0, -1)
+  .map((line) =>
+    line
+      .replaceAll('    ', '-')
+      .replaceAll(' ', '')
+      .replaceAll('[', '')
+      .replaceAll(']', '')
+      .split(''),
+  )
+  .reduce((stacks, stack) => {
+    for (let i = 0; i < stack.length; i++) {
+      if (!stacks[i]) stacks[i] = [];
+      if (stack[i] === '-') continue;
+      stacks[i].push(stack[i]);
+    }
+    return stacks;
+  }, []);
+const stacks2 = JSON.parse(JSON.stringify(stacks));
 
-const stacks2 = [
-  [...'QHCTNSVB'],
-  [...'GBDQ'],
-  [...'BQSTRWF'],
-  [...'NDJZSWGL'],
-  [...'FVDPM'],
-  [...'JWF'],
-  [...'VJBQNL'],
-  [...'NSQJCRTG'],
-  [...'MDWCQSJ'],
-];
-
-const steps = lines
+const result = stepInput
+  .split('\n')
   .map((line) => line.match(/move (\d+) from (\d+) to (\d+)/))
   .filter((match) => match != null)
   .map(([, quantity, from, to]) => ({
     quantity: Number(quantity),
     from: Number(from - 1),
     to: Number(to - 1),
-  }));
+  }))
+  .forEach((step) => {
+    const { quantity, from, to } = step;
+    for (let i = 0; i < quantity; i++) {
+      const crate = stacks[from].shift();
+      stacks[to].unshift(crate);
+    }
+    const crates = stacks2[from].splice(0, quantity);
+    stacks2[to].unshift(...crates);
+  });
 
-for (const step of steps) {
-  const { quantity, from, to } = step;
-  for (let i = 0; i < quantity; i++) {
-    const crate = stacks[from].shift();
-    stacks[to].unshift(crate);
-  }
-}
-let output = '';
-for (const stack of stacks) {
-  output += stack.shift();
-}
-
-for (const step of steps) {
-  const { quantity, from, to } = step;
-  const crates = stacks2[from].splice(0, quantity);
-  stacks2[to].unshift(...crates);
-}
-let output2 = '';
-for (const stack of stacks2) {
-  output2 += stack.shift();
-}
-
-console.log(output);
-console.log(output2);
+console.log(stacks.map((stack) => stack[0]).join(''));
+console.log(stacks2.map((stack) => stack[0]).join(''));
