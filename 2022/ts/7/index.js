d77f63083488d43818fcd820f91ad85d587833b7
Eli Richardson <elijahrich02@icloud.com>
2022-12-07 00:57:56 -0600

    day 7 completed

--- 2022/ts/7/index.js
+++ 2022/ts/7/index.js
@@ -1,3 +1,75 @@
-import { readFile } from "node:fs/promises";
+import { readFileSync } from 'node:fs';
 
-const input = await readFile("./test.txt");
+const input = readFileSync('./input.txt', 'utf-8')
+  .trim()
+  .split('$')
+  .filter((line) => line.length > 0)
+  .map((line) => line.trim())
+  .map((line) => line.split('\n'));
+
+let currentDir = '';
+const files = new Map();
+
+input.shift();
+
+for (const line of input) {
+  if (line[0].startsWith('cd')) {
+    const path = line[0].split(' ')[1].trim();
+    if (path === '..') {
+      const pathArr = currentDir.split('/');
+      pathArr.pop();
+      currentDir = pathArr.join('/');
+    } else {
+      currentDir = currentDir + '/' + path;
+    }
+  }
+  if (line[0] === 'ls') {
+    const filesInDir = line.slice(1, line.length);
+    files.set(currentDir, filesInDir);
+  }
+}
+
+files.set('/', files.get(''));
+files.delete('');
+
+const calculateSum = (currentDir) => {
+  let sum = 0;
+  const filesInDir = files.get(currentDir);
+  for (const file of filesInDir) {
+    if (file.startsWith('dir')) {
+      let subDir = file.split(' ')[1];
+      if (currentDir !== '/') subDir = currentDir + '/' + subDir;
+      else subDir = '/' + subDir;
+      sum += calculateSum(subDir);
+    } else {
+      sum += parseInt(file.split(' ')[0]);
+    }
+  }
+  return sum;
+};
+
+const result = [];
+
+for (const [dir, _] of files) {
+  result.push(calculateSum(dir));
+}
+console.log(result.filter((val) => val <= 100000).reduce((sum, val) => sum + val, 0));
+
+const MAX_FS_SIZE = 70000000;
+const NEED_UNUSED = 30000000;
+const USED = result.reduce((sum, val) => sum + val, 0);
+
+const sizes = new Map();
+for (const [dir, _] of files) {
+  sizes.set(dir, calculateSum(dir));
+}
+
+const diff = MAX_FS_SIZE - sizes.get('/');
+const needed = NEED_UNUSED - diff;
+
+// get dir with smallest size that is over needed
+const smallestDir = [...sizes.entries()]
+  .filter(([dir, size]) => size > needed)
+  .sort((a, b) => a[1] - b[1])[0];
+
+console.log(smallestDir);
