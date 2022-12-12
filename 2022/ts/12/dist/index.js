05c8d99217de65d503d147699489a28fd77acca7
Eli Richardson <elijahrich02@icloud.com>
2022-12-12 03:28:06 -0600

    remove dist and add to .gitignore

--- 2022/ts/12/dist/index.js
+++
@@ -1,5 +0,0 @@
-import { readFileSync } from 'node:fs';
-const input = readFileSync('test.txt', 'utf-8').trim().split('\n');
-const grid = input.map((line) => line.split(''));
-console.log(grid);
-//# sourceMappingURL=index.js.map
