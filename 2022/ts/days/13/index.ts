402660bafdf8a816f0923a817047d3511e93d550
Eli Richardson <elijahrich02@icloud.com>
2022-12-13 02:16:34 -0600

    add 'typescript' solution for day 13... lol

---
+++ 2022/ts/days/13/index.ts
@@ -0,0 +1,52 @@
+import { readFileSync } from 'node:fs';
+
+const chunks = readFileSync('input.txt', 'utf-8').trim().split('\n\n');
+
+const packets = chunks.map((chunk) => chunk.split('\n').map((l) => JSON.parse(l)));
+
+const compare = (left: any, right: any): any => {
+  if (typeof left === 'number' && typeof right === 'number') {
+    if (left < right) return 1;
+    if (left > right) return -1;
+    return 0;
+  }
+  if (Array.isArray(left) && Array.isArray(right)) {
+    for (let i = 0; i < left.length; i++) {
+      if (right[i] === undefined) return -1;
+      const result: any = compare(left[i], right[i]);
+      if (result) return result;
+    }
+    if (left.length < right.length) return 1;
+    else return 0;
+  }
+  if (typeof left === 'number') return compare([left], right);
+  if (typeof right === 'number') return compare(left, [right]);
+};
+
+let sum = 0;
+
+for (const packet of packets) {
+  const left = packet[0];
+  const right = packet[1];
+  const result = compare(left, right);
+  if (result === 1) sum += packets.findIndex((p) => p === packet) + 1;
+}
+// part 1 answer
+console.log(sum);
+
+// part 2
+const input = readFileSync('input.txt', 'utf-8')
+  .trim()
+  .split('\n')
+  .filter((i) => i !== '')
+  .map((i) => JSON.parse(i));
+
+input.push([[6]]);
+input.push([[2]]);
+
+const sortedPackets = input.sort((a, b) => compare(b, a));
+
+const divider1 = sortedPackets.findIndex((p) => JSON.stringify(p) === JSON.stringify([[6]])) + 1;
+const divider2 = sortedPackets.findIndex((p) => JSON.stringify(p) === JSON.stringify([[2]])) + 1;
+
+console.log(divider1 * divider2);
