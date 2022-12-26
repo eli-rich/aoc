f859a2fc8bccc636b457fade3f49b76d9b903411
Eli Richardson <elijahrich02@icloud.com>
2022-12-26 16:35:42 -0600

    d25

--- 2022/ts/days/22/index.ts
+++ 2022/ts/days/22/index.ts
@@ -1,6 +1,6 @@
-import { Dir, readFileSync } from 'fs';
-
-const [mapInput, directions] = readFileSync('input.txt', 'utf-8').split('\n\n');
+import { readFileSync } from 'fs';
+const input = readFileSync('input.txt', 'utf-8');
+const [mapInput, directions] = input.split('\n\n');
 
 type Square = {
   row: number;
@@ -66,7 +66,7 @@ const render = (map: Square[], player: Player) => {
   return result;
 };
 
-const player = {
+let player = {
   row: 1,
   col: map.find(s => s.type === 'open' && s.row === 1)!.col,
   facing: '>',
@@ -139,3 +139,80 @@ const faceScore = {
 
 const score = player.row * 1000 + player.col * 4 + faceScore[player.facing];
 console.log('row:', player.row, 'col:', player.col, 'facing:', player.facing, 'score:', score);
+
+// part 2
+// https://gist.github.com/p-a/1bcbeb838bd98e9db3b8b0ba3287d0a0
+
+const [R, D, L, U] = [0, 1, 2, 3];
+
+const wrapper = [
+  [
+    // R
+    [[Infinity, 50], ([x, y]) => [[99, 149 - y], L]],
+    [[Infinity, 100], ([x, y]) => [[y + 50, 49], U]],
+    [[Infinity, 150], ([x, y]) => [[149, 149 - y], L]],
+    [[Infinity, 200], ([x, y]) => [[y - 100, 149], U]],
+  ],
+  [
+    // D
+    [[50, Infinity], ([x, y]) => [[x + 100, 0], D]],
+    [[100, Infinity], ([x, y]) => [[49, x + 100], L]],
+    [[150, Infinity], ([x, y]) => [[99, x - 50], L]],
+  ],
+  [
+    // L
+    [[Infinity, 50], ([x, y]) => [[0, 149 - y], R]],
+    [[Infinity, 100], ([x, y]) => [[y - 50, 100], D]],
+    [[Infinity, 150], ([x, y]) => [[50, 149 - y], R]],
+    [[Infinity, 200], ([x, y]) => [[y - 100, 0], D]],
+  ],
+  [
+    // U
+    [[50, Infinity], ([x, y]) => [[50, x + 50], R]],
+    [[100, Infinity], ([x, y]) => [[0, x + 100], R]],
+    [[150, Infinity], ([x, y]) => [[x - 100, 199], U]],
+  ],
+];
+
+const DIR = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1],
+];
+
+const pipe =
+  (...fns) =>
+  v =>
+    fns.reduce((a, f) => f(a), v);
+
+const parse = (input: string) => {
+  const g = input.split('\n\n');
+  const map = g[0].split('\n').map(line => line.split('').map(c => c.trim()));
+  const instr = [...('R' + g[1]).matchAll(/(([LR])(\d+))/g)]
+    .map(([, , a, b]) => [a, b])
+    .map(([a, b]) => [a === 'L' ? -1 : 1, Number(b)]);
+  return [map, instr, [[map[0].findIndex(c => c === '.'), 0], -1], [map[0].length, map.length]];
+};
+
+const followCube = ([map, instr, start]) =>
+  instr.reduce(([p, f], [turn, steps]) => {
+    let facing = (f = (f + 4 + turn) % 4);
+    let pos, c;
+    while ((c = map[p[1]]?.[p[0]]) !== '#' && steps >= 0) {
+      if (c) {
+        pos = p;
+        facing = f;
+        steps--;
+        p = DIR[facing].map((d, i) => d + p[i]);
+      } else {
+        [p, f] = wrapper[facing].find(([b]) => p.every((c, i) => c < b[i]))[1](p);
+      }
+    }
+    return [pos, facing];
+  }, start);
+
+const scorer = ([[col, row], facing]) => 1000 * (row + 1) + 4 * (col + 1) + facing;
+const score2 = pipe(parse, followCube, scorer);
+
+console.log(score2(input));
