e6d9432c919f35740f210e0ea3c5885c71d532a0
Eli Richardson <elijahrich02@icloud.com>
2022-12-25 21:50:21 -0600

    d16

--- 2022/ts/days/16/index.ts
+++ 2022/ts/days/16/index.ts
@@ -7,12 +7,12 @@ type Valve = {
   open: boolean;
 };
 
-type Valves = Map<string, Valve>;
 // Parse the input to create a graph of valves and their flow rates
-const input = readFileSync('test.txt', 'utf-8').trim();
+const input = readFileSync('input.txt', 'utf-8').trim();
 const lines = input.split('\n');
 const valves = new Map<string, Valve>();
-
+const bit: Record<string, bigint> = {};
+let bitIndex = 0n;
 for (const line of lines) {
   const segments = line.split(' ');
   const name = segments[1];
@@ -20,68 +20,78 @@ for (const line of lines) {
   const tunnelNames = segments.slice(9).map(s => s.replace(/[,;]/g, ''));
   const valve: Valve = { name, flowRate, tunnel: tunnelNames, open: false };
   valves.set(name, valve);
+  if (valve.flowRate > 0) bit[name] = 1n << bitIndex++;
 }
 
-type Action = {
-  type: 'open' | 'travel';
-  to: string;
-  done: number;
+const vertices: string[] = [];
+for (const valve of valves.values()) {
+  vertices.push(valve.name);
+}
+
+const objectFromKeys = <T>(keys: string[], valueFn: (key: string) => T) =>
+  Object.fromEntries(keys.map(k => [k, valueFn(k)]));
+
+let distances: Record<string, Record<string, number>> = objectFromKeys(vertices, () =>
+  objectFromKeys(vertices, () => Infinity),
+);
+
+for (const vertex of vertices) {
+  distances[vertex][vertex] = 0;
+  for (const neighbor of valves.get(vertex)!.tunnel) {
+    distances[neighbor][vertex] = 1;
+  }
+}
+
+for (const k of vertices) {
+  for (const i of vertices) {
+    for (const j of vertices) {
+      if (distances[i][j] > distances[i][k] + distances[k][j]) {
+        distances[i][j] = distances[i][k] + distances[k][j];
+      }
+    }
+  }
+}
+
+for (const [from, map] of Object.entries(distances)) {
+  for (const to of Object.keys(map)) {
+    if (to === from || valves.get(to)!.flowRate === 0) delete map[to];
+  }
+}
+
+// Run DFS to find the best path
+
+const cache = new Map<string, number>();
+
+const dfs = (time: number, valve: string, open: bigint): number => {
+  const key = `${time}-${valve}-${open}`;
+  const cVal = cache.get(key);
+  if (cVal !== undefined) return cVal;
+
+  let max = 0;
+  for (const [neighbor, dist] of Object.entries(distances[valve])) {
+    if (open & bit[neighbor]) continue;
+    const remaining = time - 1 - dist;
+    if (remaining <= 0) continue;
+    const best = dfs(remaining, neighbor, open | bit[neighbor]);
+    max = Math.max(max, best + valves.get(neighbor)!.flowRate * remaining);
+  }
+  cache.set(key, max);
+  return max;
 };
 
-class Simulation {
-  public valves: Valves;
-  public currentValve: string;
-  public time: number;
-  public maxTime: number;
-  public actionQueue: Action[];
-  public pressure: number;
-  constructor(valves: Valves, currentValve: string, maxTime: number) {
-    this.valves = valves;
-    this.currentValve = currentValve;
-    this.time = 0;
-    this.maxTime = maxTime;
-    this.actionQueue = [];
-    this.pressure = 0;
-  }
-  tick() {
-    this.time++;
-    if (this.actionQueue.length !== 0 && this.actionQueue[0].done === this.time) {
-      const action = this.actionQueue.shift()!;
-      if (action.done === this.time) {
-        if (action.type === 'open') {
-          const valve = this.valves.get(action.to)!;
-          valve.open = true;
-          this.pressure += valve.flowRate * (this.maxTime - this.time);
-        } else {
-          this.currentValve = action.to;
-        }
-      }
-    }
-  }
-  open(valve: string) {
-    // add action to open valve that resolves in 1 minute
-    this.actionQueue.push({ type: 'open', to: valve, done: this.time + 1 });
-  }
-  travel(valve: string) {
-    // add action to travel to valve that resolves in 1 minute
-    this.actionQueue.push({ type: 'travel', to: valve, done: this.time + 1 });
-  }
+// part 1
+console.log(dfs(30, 'AA', 0n));
+
+// part 2
+// now there are two people
+// both start at AA
+// both have 26 minutes
+// find the max flow rate
+const ALL_OPEN = 2n ** bitIndex - 1n;
+let max = -Infinity;
+for (let i = 0n; i < ALL_OPEN; i++) {
+  const v = dfs(26, 'AA', i) + dfs(26, 'AA', ALL_OPEN ^ i);
+  max = Math.max(max, v);
 }
 
-// get distances between all valves
-const distances = new Map<string, Map<string, number>>();
-for (const [name, valve] of valves) {
-  const distanceMap = new Map<string, number>();
-  const queue = valve.tunnel.map(t => ({ name: t, distance: 1 }));
-  while (queue.length !== 0) {
-    const { name, distance } = queue.shift()!;
-    if (!distanceMap.has(name)) {
-      distanceMap.set(name, distance);
-      const nextValve = valves.get(name)!;
-      for (const tunnel of nextValve.tunnel) {
-        queue.push({ name: tunnel, distance: distance + 1 });
-      }
-    }
-  }
-  distances.set(name, distanceMap);
-}
+console.log(max);
